c7d1917f208f732d1a1865511d765cf98e1c7f46
UnknownQuantity <jacob_wr@live.dk>
2019-01-31 12:22:12 +0100

    First commit
    commit

---
+++ Assignment 1/Assignment 1 code/Assets/Scripts/ButtonHandler.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ButtonHandler : MonoBehaviour
+{
+
+
+    public Text text;
+    public GameObject spawntarget;
+    
+
+    public void TextPrinter ()
+    {
+        text.text = "Hello World";
+        Debug.Log("Hello World");
+    }
+
+    public void Spawner()
+    {
+        Instantiate(spawntarget);
+        
+    }
+    
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assignment 1/Assignment 1 code/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+    public float speed = 10f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(speed * Input.GetAxis("Horizontal") * Time.deltaTime,
+            0f, speed * Input.GetAxis("Vertical") * Time.deltaTime);
+    }
+}
